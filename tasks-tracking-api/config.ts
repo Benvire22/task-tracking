5d8b399bd5692b52f40059d008fde544c3a1f0d4
Ivan Limarev <ivantaketo@gmail.com>
2025-02-26 16:42:14 +0600

    Config server.

---
+++ tasks-tracking-api/config.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import { CorsOptions } from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const rootPath = __dirname;
+
+const corsWhiteList = process.env.CORS_WHITE_LIST?.split(",") || [];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || corsWhiteList.indexOf(origin) !== -1) {
+      callback(null, origin);
+    } else {
+      callback(new Error(`Not Allowed by CORS`));
+    }
+  },
+};
+
+const config = {
+  rootPath,
+  publicPath: path.join(rootPath, "public"),
+  corsOptions,
+  database: process.env.CORS_DATABASE_URL,
+};
+
+export default config;
---
+++ tasks-tracking-api/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import config from "@/config";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const run = async () => {
+  if (!config.database) {
+    throw new Error(
+      "DATABASE_URL is not defined in the environment variables.",
+    );
+  }
+
+  await mongoose.connect(config.database);
+
+  app.listen(port, () => {
+    console.log(`started on ${port} port`);
+  });
+
+  process.on("exit", () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
